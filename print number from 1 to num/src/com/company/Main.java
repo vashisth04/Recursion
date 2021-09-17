cdcb289892eb58c20ce9f78013df00731ce317b5
vashisth04 <60615838+vashisth04@users.noreply.github.com>
2021-09-17 19:13:56 +0530

    Add files via upload

---
+++ print number from 1 to num/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        fun(1);
+    }
+    public static void fun(int n){
+        if(n>10){
+            return;
+        }
+        System.out.println(n);
+        fun(n+1);
+    }
+}
